6c15746ee30053519e5c93410462e30569b50e21
Igor <igor@MacBook-Air-Igor.local>
2023-05-03 21:27:02 -0700

    Using customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        'layout': 'auto 1fr auto',
+      },
+      colors: {
+        'logo-main': '#9b0404',
+        'logo-main-dark': '#A85524',
+      },
+      fontFamily: {
+        main: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 }
