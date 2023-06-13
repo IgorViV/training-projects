4033996ca7b20feacd21235067a3aca13b76a479
Igor <igor@MacBook-Air-Igor.local>
2023-06-13 10:47:02 +0300

    Добавляет вспомогательные функции и исправляет getRandomNumberForRange и isStringShort

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,42 @@
 /**
  * Функция, возвращающая случайное целое число из переданного диапазона включительно.
  *
- * Учтите, что диапазон может быть только положительный, включая ноль.
- * А также придумайте, как функция должна вести себя, если передать значение «до» меньшее,
- * чем значение «от», или равное ему.
+ * @param {Number} min Первое число диапазона
+ * @param {Number} max Второе число диапазона
+ * @return {Number} Случайное целое число
  */
 function getRandomNumberForRange(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
+    min = Math.ceil(Math.abs(min));
+    max = Math.floor(Math.abs(max));
+
+    if (min === max) {
+        return min;
+    }
+
+    if (min > max) {
+        return Math.floor(Math.random() * (min - max + 1)) + max;
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomNumberForRange(1, 20);
+getRandomNumberForRange(1, 20); // Для исключения ошибки линтера
 
 /**
  * Функция для проверки максимальной длины строки.
+ *
+ * @param {String} string Проверяемая строка
+ * @param {Number} maxLength Допустимая максимальная длина строки
+ * @return {boolean}
  */
 function isStringShort(string, maxLength) {
     const symbols = [...string];
+    maxLength = Math.ceil(Math.abs(maxLength));
+
     if (symbols.length > maxLength) {
         return false;
     }
     return true;
 }
 
-isStringShort('good', 100);
+isStringShort('good', 100); // Для исключения ошибки линтера
